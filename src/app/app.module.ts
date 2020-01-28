975cfe17868ed414155847429e8285367189f2dc
Michal Dziadkowiec <michaldziadkowiec@yahoo.pl>
2020-01-28 18:22:57 +0100

    Add edit form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './store/app.reducer';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-database/in-memory-data.service';
-import { HttpClientModule } from '@angular/common/http';
-import { UsersComponent } from './users/users.component';
-import { UsersListComponent } from './users/users-list/users-list.component';
-import { LoaderComponent } from './shared/components/loader/loader.component';
-import { UserDetailsComponent } from './users/user-details/user-details.component';
-import { EffectsModule } from '@ngrx/effects';
-import { UsersEffects } from './users/store/users.effects';
-import { ImgLoadedComponent } from './shared/components/img-loaded/img-loaded.component';
-import { CardComponent } from './shared/components/card/card.component';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { DataTableComponent } from './shared/components/data-table/data-table.component';
-import { CarsComponent } from './cars/cars.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { CarsListComponent } from './cars/cars-list/cars-list.component';
-@NgModule({ 
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { StoreModule } from "@ngrx/store";
+import { reducers, metaReducers } from "./store/app.reducer";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-database/in-memory-data.service";
+import { HttpClientModule } from "@angular/common/http";
+import { UsersComponent } from "./users/users.component";
+import { UsersListComponent } from "./users/users-list/users-list.component";
+import { LoaderComponent } from "./shared/components/loader/loader.component";
+import { UserDetailsComponent } from "./users/user-details/user-details.component";
+import { EffectsModule } from "@ngrx/effects";
+import { UsersEffects } from "./users/store/users.effects";
+import { ImgLoadedComponent } from "./shared/components/img-loaded/img-loaded.component";
+import { CardComponent } from "./shared/components/card/card.component";
+import { NgxDatatableModule } from "@swimlane/ngx-datatable";
+import { DataTableComponent } from "./shared/components/data-table/data-table.component";
+import { CarsComponent } from "./cars/cars.component";
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { CarsListComponent } from "./cars/cars-list/cars-list.component";
+import { ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
@@ -56,8 +58,10 @@ import { CarsListComponent } from './cars/cars-list/cars-list.component';
       logOnly: environment.production
     }),
     EffectsModule.forRoot([UsersEffects]),
-    NgxDatatableModule
+    NgxDatatableModule,
+    ReactiveFormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/users/user-details/user-details.component.ts
+++ src/app/users/user-details/user-details.component.ts
@@ -1,30 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import * as fromApp from '../../store/app.reducer';
-import * as UsersActions from '../store/users.actions';
-import * as UsersSelectors from '../store/users.selectors';
-import { ActivatedRoute, Params } from '@angular/router';
-import { map } from 'rxjs/operators';
-import { User } from 'src/app/shared/models/user.model';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import * as fromApp from "../../store/app.reducer";
+import * as UsersActions from "../store/users.actions";
+import * as UsersSelectors from "../store/users.selectors";
+import { ActivatedRoute, Params } from "@angular/router";
+import { map, skip, take, mergeMap } from "rxjs/operators";
+import { User } from "src/app/shared/models/user.model";
+import { FormGroup, FormControl } from "@angular/forms";
+import { of, Observable, forkJoin } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+
+const AVAILABLE_CARS = [
+  {
+    id: 1,
+    name: "Opel Astra"
+  },
+  {
+    id: 2,
+    name: "Fiat Punto"
+  },
+  {
+    id: 3,
+    name: "Audi A6"
+  }
+];
+
 @Component({
-  selector: 'app-user-details',
-  templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss']
+  selector: "app-user-details",
+  templateUrl: "./user-details.component.html",
+  styleUrls: ["./user-details.component.scss"]
 })
 export class UserDetailsComponent implements OnInit {
   user: User;
   isLoading = true;
   isError = false;
+  userEditForm: FormGroup;
+  availableCars = AVAILABLE_CARS;
 
-  constructor(private store: Store<fromApp.AppState>, private route: ActivatedRoute) { }
+  constructor(
+    private store: Store<fromApp.AppState>,
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
-    this.store.pipe(
-      map((state) => UsersSelectors.selectUser(state)))
-        .subscribe(userData => {
-          this.user = userData.user;
-          this.isLoading = userData.isLoading;
-          this.isError = userData.isError;
+    this.experimentalMethod();
+
+    this.createForm();
+
+    this.store
+      .pipe(map(state => UsersSelectors.selectUser(state)))
+      .subscribe(userData => {
+        this.user = userData.user;
+        this.isLoading = userData.isLoading;
+        this.isError = userData.isError;
       });
 
     this.route.params.subscribe((params: Params) => {
@@ -33,7 +62,37 @@ export class UserDetailsComponent implements OnInit {
     });
   }
 
+  private createForm(): void {
+    this.userEditForm = new FormGroup({
+      firstName: new FormControl(""),
+      lastName: new FormControl(""),
+      car: new FormControl("")
+    });
+  }
+
   private getUserDetailsById(id: number): void {
     this.store.dispatch(UsersActions.loadUserDetails({ id }));
+  }
+
+  public onCarChange(value: string): void {
+    console.log(value);
+  }
+
+  // Experiments with rxjs
+
+  private getCars(): Observable<any> {
+    return this.http.get<any>("/api/cars");
+  }
+
+  private getUsers(): Observable<any> {
+    return this.http.get<any>("/api/users");
+  }
+
+  public experimentalMethod(): void {
+    forkJoin([this.getCars(), this.getUsers()]).subscribe(results => {
+      const [cars, users] = results;
+
+      console.log(cars, users);
+    });
   }
 }
